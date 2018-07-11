59e8a02ace70f26162da24e98d1a8519524f1754
jessieshao <505133853@qq.com>
2018-07-11 22:50:39 +0800

    commit

--- node-demo/server.js
+++ node-demo/server.js
@@ -10,57 +10,33 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-
-  console.log('HTTP路径为\n' + path)
-  if (path == '/'){
-    response.setHeader('content-type','text/html; Unicode')
-    response.write('<DOCTYPE>\n<html>' + 
-    '<head><link rel="StyleSheet" href="/style.css">' +
-    '</head><body>' + 
-    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'+
-    '<h1>你好</h1>' +
-    '<script src="/main.js"></script>' +
-    '</body></html>')
-    response.end()
-  }else if(path == '/style.css'){
-    response.setHeader('content-type','text/css; Unicode')
-    response.write('body{background-color :#ddd;} h1{color:red;}')
-    response.end()
-  }else if(path == '/main.js'){
-    response.setHeader('content-type','text/javascript; Unicode')
-    response.write('alert("这是js执行的")')
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
     response.end()
   }else{
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
     response.end()
   }
-
-
-
-
-
-
-
-
-
-
-
-
 
   /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
 
 
